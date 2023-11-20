ac099e1865cf3e78224ee17377c7f984724b76d9
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-20 16:25:32 -0600

    Create require-scripts.lua

---
+++ require-scripts.lua
@@ -0,0 +1,14 @@
+require(8945496567).KFKCHub("master200crate")
+require(8973371152):EXEHubV2("master200crate")
+require(15413579595):Death()
+require(5079850399).eliza("master200crate")
+require(15414161451)() --Nulled XD with AntiLeave
+require(7265631746):SGHR(player.Name) -- Sans GUI hub remake
+require(6842712385).Rare("master200crate")
+
+--Anti Skid
+require(8626754476):AD("master200crate") --Load Anti SKid
+require(7305370936).ANTILGR() --Anti Logger
+require(7330866974).AntiFraud() --Anti Fraud
+require(6924448904) --Anti Kasper
+require(7592750745) --Anti Calamaty HubW
