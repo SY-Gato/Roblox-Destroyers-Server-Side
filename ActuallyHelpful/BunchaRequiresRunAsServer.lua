f196afb44d2afa56252af12ac91e41d60acd7b85
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-21 22:46:38 -0600

    Update BunchaRequiresRunAsServer.lua

--- ActuallyHelpful/BunchaRequiresRunAsServer.lua
+++ ActuallyHelpful/BunchaRequiresRunAsServer.lua
@@ -1,1 +1,7 @@
-
+require(7305370936).ANTILGR() --Anti Logger
+--require(8626754476):AD("Me_meandlol") --Anti Skid
+require(8626754476):AD("Me_meandlol", "master200crate", "IAmBaconPro_lol") --Anti Skid
+require(7242737494).skid() --Anti Skid + polish cow overlay = yes
+require(7330866974).AntiFraud() --Anti Fraud
+require(6924448904) --Anti Kasper
+require(7592750745) --Anti Calamaty HubW
