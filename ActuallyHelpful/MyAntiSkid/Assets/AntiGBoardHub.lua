7175a61125fb6085e705978a80f5e8763c39e56f
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-23 17:54:55 -0600

    Update AntiGBoardHub.lua

--- ActuallyHelpful/MyAntiSkid/Assets/AntiGBoardHub.lua
+++ ActuallyHelpful/MyAntiSkid/Assets/AntiGBoardHub.lua
@@ -1,7 +1,7 @@
 function ScanForHub()
   for i,v in pairs(game.Players:GetPlayers()) do
     for l,s in pairs(v.PlayerGui:GetDescendants()) do
-      if s.Name = "LivingInTheLight" and s:FindFirstChild("TheCure") ~= nil then
+      if s.Name == "LivingInTheLight" and s:FindFirstChild("TheCure") ~= nil then
         for h,d in pairs(s.Parent:GetDescendants()) do
           if d:IsA("Script") or d:IsA("LocalScript") then
             d.Disabled = true
