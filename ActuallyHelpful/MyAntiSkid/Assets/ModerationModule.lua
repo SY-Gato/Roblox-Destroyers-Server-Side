d8e2098adca274568f07de4142f26e66eead7001
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-24 14:19:33 -0600

    Update ModerationModule.lua

--- ActuallyHelpful/MyAntiSkid/Assets/ModerationModule.lua
+++ ActuallyHelpful/MyAntiSkid/Assets/ModerationModule.lua
@@ -1,4 +1,17 @@
 local module = {}
 
+local newline = string.char(13)
+local HttpService = game:GetService("HttpService")
+
+local function randomizeName(obj)
+  local rndmname = HttpService:GenerateGUID(false)
+  obj.Name = rndmname
+end
+
+local function SafeKickNoTraces(plr, reason)
+  local plr = game.Players:FindFirstChild(plr)
+  if plr then
+    if plr:FindFirstChild("Backpack")
+end
 
 return module
