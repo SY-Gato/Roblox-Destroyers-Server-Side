49f0d1adb24f0bd52c2c25ccb850ea5bf1985cd8
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-23 21:35:13 -0600

    Update AntiGBoardHub.lua

--- ActuallyHelpful/MyAntiSkid/Assets/AntiGBoardHub.lua
+++ ActuallyHelpful/MyAntiSkid/Assets/AntiGBoardHub.lua
@@ -19,13 +19,26 @@ function ScanForHub2()
   for i,v in pairs(game.Players:GetPlayers()) do
     for l,s in pairs(v.PlayerGui:GetDescendants()) do
       --if s.Name = "Hub" and s:FindFirstChild("Hub Frame") ~= nil or s.Name = "Hub Frame" then
-      if s.Name = "Hub Frame" and s.Parent:FindFirstChild("Skid Shield") and 
-        
+      if s.Name = "HubFrame" and s.Parent:FindFirstChild("Rainbow LC") then
+        for h,d in pairs(s.Parent:GetDescendants()) do
+          if d:IsA("Script") or d:IsA("LocalScript") then
+            d.Disabled = true
+          end
+        end
+        s.Parent.Enabled = false
+        warn(tostring(s.Parent.Parent.Parent.Name))
+        s.Parent.Parent = game.ReplicatedStorage
+        s:Destroy()
+      end
+    end
+  end
+end
       
 
 wait(15)
 
 while true do
   ScanForHub1()
+  ScanForHub2()
   wait(10)
 end
