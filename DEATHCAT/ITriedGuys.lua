351754547011daa34a9e2d33f9d4c08f2c1ebe3a
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-24 19:51:50 -0600

    Create ITriedGuys.lua

---
+++ DEATHCAT/ITriedGuys.lua
@@ -0,0 +1,35 @@
+local StarterGui = game:GetService("StarterGui")
+
+--local Msgresponse = nil
+
+function sendCoreMsg(title, text, button1, duration) --button2, duration)
+  --local bindable = Instance.new("BindableFunction")
+  StarterGui:SetCore("SendNotification", {
+  Title = title,
+	Text = text,
+	Duration = duration,
+	--Callback = bindable,
+	Button1 = button1,
+	--Button2 = button2  
+  })
+  --[[function bindable.OnInvoke(response)
+    Msgresponse = response
+    print(response .. " chosen")
+  end]]
+end
+
+SendCoreMsg("DeathCat", "you run DeathCat", "yipeeee", 20) -- Script startup msg ???
+
+local RepStorage = game:GetService("ReplicatedStorage")
+local plrs = game:GetService("Players")
+
+
+local plr = plrs.LocalPlayer
+local char = plr.Character
+
+
+function charnotright()
+  char:MoveTo(Vector3.new(9999999,9999999,9999999))
+  for i,v in pairs(char:GetChildren()) do
+    if v:IsA("Part") or v:IsA("MeshPart") then
+      v.Anchored = true
