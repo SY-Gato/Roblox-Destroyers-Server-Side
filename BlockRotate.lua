5ca7dcedb217757c2ab85207974ecc2fc3b2a9cf
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-25 16:15:14 -0600

    Update BlockRotate.lua

--- BlockRotate.lua
+++ BlockRotate.lua
@@ -1,1 +1,18 @@
 
+
+local SPIN_REPETITIONS = 20
+local SPIN_SPEED = 10
+
+function Spin()
+  for i = 0,SPIN_REPETITIONS do
+    for _, v in pairs(workspace:GetDescendants()) do
+      if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("Truss") or v:IsA("WedgePart") then
+        if game.Players:GetPlayerFromCharacter(v.Parent) == nil then
+          v.Rotation *= CFrame.Angles(0.2, 0.2, 0.2)
+        end
+      end
+    end
+  end
+end
+wait(10)
+Spin()
