3a4a03cfd3bfd4585adbc3305a511ed76cfb61fc
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-27 10:49:18 -0600

    Update CommandsTest1.lua

--- CommandsUI/CommandsTest1.lua
+++ CommandsUI/CommandsTest1.lua
@@ -1,7 +1,23 @@
 if game:GetService("RunService"):IsClient() then error("Run As SERVER") end
 
+-- Services
 local RunServ = game:GetService("RunService")
 local Plrs = game:GetService("Players")
 local HttpServ = game:GetService("HttpService")
 
+-- Variables
+wait(3) -- For player loading time
+local plr = Plrs.LocalPlayer
+local char = plr.Character
+local hum = char:FindFirstChildOfClass("Humanoid")
+local hrp = char:FindFirstChild("HumanoidRootPart")
+
+-- Variables for searching and syntax
 local Priority = "DisplayName"
+local Syntax = "" -- Currently No Syntax
+local Whitelist = {"master200crate"}
+local IgnoreWhitelist = true
+
+-- Command Function
+local function KickPlr(plr, reason)
+  if Priority == Dospla
