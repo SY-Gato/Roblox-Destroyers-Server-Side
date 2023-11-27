8b71144160fb4456f62d02d57b1ef5240ca6f3c4
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-11-27 10:24:47 -0600

    Create CommandsTest1.lua

---
+++ CommandsUI/CommandsTest1.lua
@@ -0,0 +1,7 @@
+if game:GetService("RunService"):IsClient() then error("Run As SERVER") end
+
+local RunServ = game:GetService("RunService")
+local Plrs = game:GetService("Players")
+local HttpServ = game:GetService("HttpService")
+
+local Priority = "DisplayName"
